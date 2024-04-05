b3d21f7e2189e1ce7b7523239452684617b7236d
asm2212 <asmareadmasu0@gmail.com>
2024-04-05 12:28:45 +0300

    componet

--- weather-web-app/src/App.js
+++ weather-web-app/src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
+import TopButton from './components/TopButton';
+import UilReact from '@iconscout/react-unicons/icons/uil-react'
 
 function App() {
   return (
-    <div className="App">
-    <h1 className='text-center'>Welcome to weather web app!</h1>
-
+    <div className="mx-auto max-w-screen-md mt-4 py-5 px-33 bg-gradient-to-br
+    from-cyan-600 to-blue-600 h-fit shadow-xl shadow-gray-400">
+     <TopButton />
     </div>
   );
 }
