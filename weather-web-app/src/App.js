7b1dfcc685e08d0a36cb855cdb7d97bd947a0a35
asm2212 <asmareadmasu0@gmail.com>
2024-04-05 12:41:51 +0300

    icons

--- weather-web-app/src/App.js
+++ weather-web-app/src/App.js
@@ -2,12 +2,15 @@
 import './App.css';
 import UilReact from '@iconscout/react-unicons/icons/uil-react'
 import CurrentWeather from './components/CurrentWeather';
+import ForecastSearch from './components/ForecastSearch';
 
 function App() {
   return (
     <div className="mx-auto max-w-screen-md mt-4 py-5 px-33 bg-gradient-to-br
     from-cyan-600 to-blue-600 h-fit shadow-xl shadow-gray-400">
       <CurrentWeather />
+      <ForecastSearch />
+    
     </div>
   );
 }
---
+++ weather-web-app/src/components/WeatherIcons.jsx
@@ -0,0 +1,30 @@
+// WeatherIcons.js
+import React from 'react';
+import { RiSunFill, RiRainFill, RiCloudFill } from 'react-icons/ri'; // Import icons from React Icons library
+
+function WeatherIcons({ condition }) {
+    let icon;
+
+    // Determine which icon to display based on weather condition
+    switch (condition) {
+        case 'sunny':
+            icon = <RiSunFill />;
+            break;
+        case 'rainy':
+            icon = <RiRainFill />;
+            break;
+        case 'cloudy':
+            icon = <RiCloudFill />;
+            break;
+        default:
+            icon = <RiSunFill />;
+    }
+
+    return (
+        <div className="text-white">
+            {icon}
+        </div>
+    );
+}
+
+export default WeatherIcons;
